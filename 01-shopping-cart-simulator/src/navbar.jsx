8b3ed2e6aa5ebb2eae74cc9f5b60bb2b123dab54
Pe-Guedss <pedro.og2002@gmail.com>
2022-02-10 12:26:32 -0300

    Creating a NavBar component.

---
+++ 01-shopping-cart-simulator/src/navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() { 
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-md">
+                    <a className="navbar-brand" href="#">
+                        Navbar
+                    </a>
+                </div>
+            </nav>
+        );
+    }
+}
+ 
+export default NavBar;
