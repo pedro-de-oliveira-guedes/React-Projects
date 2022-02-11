9c9015dc81b1cc30278e9b7f354adb4ab2bd0961
Pe-Guedss <pedro.og2002@gmail.com>
2022-02-11 08:31:55 -0300

    State Lifting the counters properties to App.js and passing them as parameters.

--- 01-shopping-cart-simulator/src/components/counters.jsx
+++ 01-shopping-cart-simulator/src/components/counters.jsx
@@ -2,51 +2,21 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = {
-        counters: [
-                    {id: 1, value: 0},
-                    {id: 2, value: 0},
-                    {id: 3, value: 5},
-                    {id: 4, value: 0},
-                ]
-    }
+    render() {
+        const { onReset, onDelete, onIncrement, counters } = this.props;
 
-    handleDelete = (counterId) => {
-        const counters = this.state.counters.filter(counter => counter.id !== counterId);
-        this.setState({ counters })
-    };
-
-    handleIncrement = (counter) => {
-        console.log("Let me take you on a little trip", counter);
-        const index = this.state.counters.indexOf(counter);
-        const counters = [...this.state.counters];
-
-        counters[index] = {...counter};
-        counters[index].value++;
-        this.setState({ counters });
-    };
-
-    handleReset = () => {
-        const counters = this.state.counters.map(counter => {
-            counter.value = 0;
-            return counter;
-        });
-        this.setState({ counters });
-    };
-
-    render() { 
         return (
         <div>
-            <button onClick={this.handleReset}
+            <button onClick={onReset}
                     className="btn btn-primary btn-md m-4">
                 Reset the counters
             </button>
 
-            { this.state.counters.map( counter => 
+            {counters.map( counter => 
                                     (<Counter key={counter.id}
-                                              onDelete={this.handleDelete}
+                                              onDelete={onDelete}
                                               counter={counter}
-                                              onIncrement={this.handleIncrement} />) 
+                                              onIncrement={onIncrement} />) 
                                     )}
         </div>
         );
