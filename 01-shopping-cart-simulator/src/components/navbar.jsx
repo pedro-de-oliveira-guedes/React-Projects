5505ee494e26cdbbdedc794d0c1cbc60d8201c82
Pe-Guedss <pedro.og2002@gmail.com>
2022-02-11 08:30:53 -0300

    Changing the NavBar to a stateless component.

--- 01-shopping-cart-simulator/src/components/navbar.jsx
+++ 01-shopping-cart-simulator/src/components/navbar.jsx
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-md">
-                    <a className="navbar-brand" href="#">
-                        Navbar
-                    </a>
-                </div>
-            </nav>
-        );
-    }
-}
+const NavBar = ({ totalCounters }) => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-md">
+                <a className="navbar-brand" href="#">
+                    Navbar
+                </a>
+                <span className="badge badge-pill badge-secondary">
+                    {totalCounters}
+                </span>
+            </div>
+        </nav>
+    );
+};
  
 export default NavBar;
