d300a23b6a297ba8fa58964ea432e8167d2c1b94
Pe-Guedss <pedro.og2002@gmail.com>
2022-09-08 01:01:25 -0300

    Calling the pokemon API every time the list is loaded.

--- sandbox/src/components/pokemons.js
+++ sandbox/src/components/pokemons.js
@@ -3,6 +3,17 @@ import { useState, useEffect } from 'react'
 const FETCH_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=10'
 
 export function PokeList(params) {
+
+    const [pokemons, setPokemons] = useState([]);
+
+    useEffect( () => {
+        fetch(FETCH_URL)
+        .then( (res) => res.json() )
+        .then( (jsonRes) => {
+            console.log(jsonRes);
+        } )
+    } );
+
     return (
         <div className='poke-list'>
             <h1 className='poke-list-name'>
